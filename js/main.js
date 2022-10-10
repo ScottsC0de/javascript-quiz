35a61c7c997f5142272ad9da9cb4fb63421eb726
scottsc0de <scott5902@gmail.com>
2022-10-10 09:40:43 -0400

    added quiz question array

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// array of quiz questions to be looped through
+var questionsArray = [{
+    question: "Question 1: How do you write comments in JavaScript?",
+    answers: {
+        a: "#",
+        b: "//",
+        c: "REM",
+        d: "None of the above"
+    },
+    correctAnswer: "b"
+},
+{
+    question: "Question 2: Which of the following are not JavaScript data types?",
+    answers: {
+        a: "Numbers",
+        b: "Strings",
+        c: "Booleans",
+        d: "Attributes"
+    },
+    correctAnswer: "d"
+},
+
+{
+    question: "Question 3: Which of the following is the correct way to store data in a variable?",
+    answers: {
+        a: "var x = 0",
+        b: "x = 0",
+        c: "x = var 0",
+        d: "var x + 0"
+    },
+    correctAnswer: "a"
+},
+
+{
+    question: "Question 4: How can you access elements in an array?",
+    answers: {
+        a: "Elements",
+        b: "Properties",
+        c: "Index",
+        d: "Variables"
+    },
+    correctAnswer: "c"
+},
+
+{
+    question: "Question 5: What is a reusable block of code in JavaScript?",
+    answers: {
+        a: "Variable",
+        b: "Function",
+        c: "Object",
+        d: "Array"
+    },
+    correctAnswer: "b"
+},];
